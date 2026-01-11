18b216fb4127fcdc579c13bd779b303fd24e8dd1
Your Name <your@email.com>
2026-01-11 13:06:58 +0530

    edited outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,21 @@
 output "public_subnets" {
-  value       = module.vpc.public_subnets
+  value = module.vpc.public_subnets
   description = "this is public subnet"
 }
 
 output "vpcname" {
-  value       = module.vpc.name
+  value = module.vpc.name
   description = "this is the name of vpc"
+}
+
+output "cluster_name" {
+  value = module.eks.cluster_name
+}
+
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
 }
